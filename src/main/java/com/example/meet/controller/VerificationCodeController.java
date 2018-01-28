63e50213afdf6f7cd996470845089b7fb92c4636
sheat <sheat120@sina.com>
2018-01-28 16:03:09 +0800

    发送验证码

--- src/main/java/com/example/meet/controller/VerificationCodeController.java
+++ src/main/java/com/example/meet/controller/VerificationCodeController.java
@@ -1,19 +1,50 @@
 package com.example.meet.controller;
 
-import com.example.meet.model.User;
+import com.example.meet.dao.VerifyCodeMapper;
+import com.example.meet.model.VerifyCode;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import javax.annotation.Resource;
+import java.util.Random;
 
+/**
+ * 验证码
+ */
 @RestController
 @RequestMapping("/verification")
 public class VerificationCodeController {
+    @Resource
+    private VerifyCodeMapper verifyCodeMapper;
 
-    @RequestMapping(value = "/")
+    /**
+     * 获取验证码
+     */
+    @RequestMapping(value = "/phone/{phone}")
     @ResponseBody
-    public String getAllUser() {
-        return "";
+    public String getVerificationCode(@PathVariable String phone) {
+        /* 生成6位随机数字 */
+        Random random = new Random();
+        String result = "";
+        for (int i = 0; i < 6; i++) {
+            result += random.nextInt(10);
+        }
+
+        /* 如果验证码记录已存在，则更新，否则新增 */
+        VerifyCode verifyCode = new VerifyCode();
+        verifyCode.setVerifyCode(result);
+        verifyCode.setTel(phone);
+
+        Integer count = verifyCodeMapper.selectCountByPhone(phone);
+
+        if (count > 0) {
+            verifyCodeMapper.updateByTel(verifyCode);
+        } else {
+            verifyCodeMapper.insert(verifyCode);
+        }
+
+        return result;
     }
 }
--- src/main/java/com/example/meet/dao/VerifyCodeMapper.java
+++ src/main/java/com/example/meet/dao/VerifyCodeMapper.java
@@ -2,11 +2,15 @@ package com.example.meet.dao;
 
 import com.example.meet.model.VerifyCode;
 import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VerifyCodeMapper {
     int deleteByPrimaryKey(Long id);
 
-    @Insert("INSERT INTO verify_code(tel,verify_code,used,create_time) VALUES(#{tel},#{verifyCode},0,NOW())")
+    @Insert("INSERT INTO verify_code(tel,verify_code,used,code_expires,create_time) VALUES(#{tel},#{verifyCode},0,date_add(NOW(), interval 30 minute),NOW())")
     int insert(VerifyCode record);
 
     int insertSelective(VerifyCode record);
@@ -15,5 +19,11 @@ public interface VerifyCodeMapper {
 
     int updateByPrimaryKeySelective(VerifyCode record);
 
-    int updateByPrimaryKey(VerifyCode record);
+    @Update("update verify_code set verify_code = #{verifyCode},code_expires = date_add(NOW(), interval 30 minute) where tel = #{tel}")
+    int updateByTel(VerifyCode record);
+
+    @Select("select count(*) from verify_code where tel = #{phone}")
+    Integer selectCountByPhone(String phone);
+
+
 }
